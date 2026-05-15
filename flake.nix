5c1ca74ea37ae4683a21cc612e0d443417637bb4
srcres258 <src.res.211@gmail.com>
2026-05-16 00:36:14 +0800

    Add Nix flake
    Signed-off-by: srcres258 <src.res.211@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "A high-performance VCD/FST waveform query CLI for AI Agents and humans";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "waveql";
+          version = "0.1.0";
+
+          src = pkgs.lib.cleanSource ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          meta = with pkgs.lib; {
+            description = "A high-performance VCD/FST waveform query CLI for AI Agents and humans";
+            license = licenses.mit;
+            mainProgram = "waveql";
+          };
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/waveql";
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+
+          packages = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+        };
+      });
+}
