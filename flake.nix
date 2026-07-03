accae1f8b1dadd6de59abafffe71fc37f5ba62a2
srcres258 <src.res.211@gmail.com>
2026-07-03 18:13:00 +0800

    waveql 0.3.0
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "waveql";
-          version = "0.2.0";
+          version = "0.3.0";
 
           src = pkgs.lib.cleanSource ./.;
 
